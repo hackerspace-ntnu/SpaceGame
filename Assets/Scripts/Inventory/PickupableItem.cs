328ffdf8388c65c8c7cf6d6062b35612a57dcb1c
Emilorv <emilols.eo@gmail.com>
2026-01-25 21:48:41 +0100

    Make PickupableItem extend IInteractable

--- Assets/Scripts/Inventory/PickupableItem.cs
+++ Assets/Scripts/Inventory/PickupableItem.cs
@@ -2,9 +2,26 @@
 
 using UnityEngine;
 
-class PickupableItem : MonoBehaviour
+class PickupableItem : MonoBehaviour, IInteractable
 {
    [SerializeField] private InventoryItem item;
-   
-   
+
+
+   public bool CanInteract()
+   {
+      return true;
+   }
+
+   public void Interact(Interactor interactor)
+   {
+      InventoryComponent inventoryComponent = interactor.GetComponent<InventoryComponent>();
+      if (inventoryComponent != null)
+      {
+         bool added = inventoryComponent.TryAddItem(item);
+         if (added)
+         {
+            Destroy(gameObject);
+         }
+      }
+   }
 }

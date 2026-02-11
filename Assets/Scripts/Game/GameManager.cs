99b36bf9df6cb4e09df314054013f9e417eda860
Emilorv <emilols.eo@gmail.com>
2026-02-11 22:01:00 +0100

    initialize game manager

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public enum GameState { Playing, Paused, Won }
+    public GameState CurrentState { get; private set; } = GameState.Playing;
+
+    public float GameTimer { get; private set; } = 0f;
+
+    public event Action<GameState> OnStateChanged;
+    
+    [SerializeField] private SceneReference onWinScene;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (CurrentState == GameState.Playing)
+            GameTimer += Time.deltaTime;
+    }
+
+    public void SetState(GameState state)
+    {
+        CurrentState = state;
+        OnStateChanged?.Invoke(state);
+    }
+
+    public void WinGame()
+    {
+        SetState(GameState.Won);
+        SceneManager.LoadScene(onWinScene.SceneName);
+    }
+}

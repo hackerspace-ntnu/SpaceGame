d78b8dc26aeb503f8ab7ddebc5731dd83a6cef28
Intikus <realintikus@gmail.com>
2026-02-19 19:38:27 +0100

    FMOD UnityModule Insertion
    Though i gotta rewrite the audiomanager there into an FMOD audiomanager.

---
+++ Assets/Scripts/Audio/AudioTestThingy.cs
@@ -0,0 +1,17 @@
+using FMODUnity;
+using UnityEngine;
+
+public class AudioTestThingy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //RuntimeManager.PlayOneShot("event:/Music/TestSong"); Works!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

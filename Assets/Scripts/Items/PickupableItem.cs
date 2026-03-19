1e858f6bac935de3f5646bdd49ae8ea3a506a624
Emilorv <emilols.eo@gmail.com>
2026-03-19 01:13:47 +0100

    network sync pickupable item

--- Assets/Scripts/Items/PickupableItem.cs
+++ Assets/Scripts/Items/PickupableItem.cs
@@ -1,23 +1,37 @@
 
 
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
 /// Script to be attached to pickupable items in the world.
 /// When interacted with, it will attempt to add the item to the player's inventory and destroy itself if successful.
 /// </summary>
-class PickupableItem : MonoBehaviour, IInteractable
+class PickupableItem : NetworkBehaviour, IInteractable
 {
    [SerializeField] private InventoryItem item;
-
-
+   
    public bool CanInteract()
    {
       return true;
    }
 
    public void Interact(Interactor interactor)
+   {
+      Network.Execute(
+         local: () => Pickup(interactor),
+         client: () => RequestPickupServerRpc(interactor.GetComponent<NetworkObject>()));
+   }
+
+   [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+   private void RequestPickupServerRpc(NetworkObjectReference interactorRef)
+   {
+      if (!interactorRef.TryGet(out NetworkObject player)) return;
+      Pickup(player.GetComponent<Interactor>());
+   }
+
+   private void Pickup(Interactor interactor)
    {
       IPlayerInventory inventory = interactor.GetComponent<IPlayerInventory>();
       if (inventory == null) return;
